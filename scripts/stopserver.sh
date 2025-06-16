2f47b112c7847569cecf42919b21e4c1b862dfb5
dhaneesh <dhaneeshjadhav@gmail.com>
2025-06-17 04:47:26 +0900

    dhanee

--- scripts/stopserver.sh
+++ scripts/stopserver.sh
@@ -6,5 +6,5 @@ LOG_FILE="/tmp/deploy.log"
 echo "=== [stopserver.sh] Stopping Tomcat service ===" >> "$LOG_FILE"
 
 #sudo systemctl stop tomcat.service
-sudo -u tomcat /opt/tomcat/bin/shutdown.sh
+sudo /opt/tomcat/bin/shutdown.sh
 echo "=== [stopserver.sh] Tomcat stopped ===" >> "$LOG_FILE"
