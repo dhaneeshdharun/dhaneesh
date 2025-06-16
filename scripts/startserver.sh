e3226467442486a9a114e8a7136c064de47b58fa
dhaneesh <dhaneeshjadhav@gmail.com>
2025-06-17 06:23:05 +0900

    dhan

--- scripts/startserver.sh
+++ scripts/startserver.sh
@@ -4,7 +4,8 @@ set -e
 LOG_FILE="/tmp/deploy.log"
 
 echo "=== [startserver.sh] Starting Tomcat service ===" >> "$LOG_FILE"
-
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk
+export PATH=$JAVA_HOME/bin:$PATH
 sudo  /opt/bin/startup.sh
 
 echo "=== [startserver.sh] Tomcat started ===" >> "$LOG_FILE"
