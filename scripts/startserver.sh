6e7654393eeb0a432f2b9a99f68105e96bb786c8
dhaneesh <dhaneeshjadhav@gmail.com>
2025-06-17 04:38:26 +0900

    dhanee

--- scripts/startserver.sh
+++ scripts/startserver.sh
@@ -5,6 +5,6 @@ LOG_FILE="/tmp/deploy.log"
 
 echo "=== [startserver.sh] Starting Tomcat service ===" >> "$LOG_FILE"
 
-sudo -u tomcat /opt/tomcat/bin/startup.sh
+sudo  sh /opt/tomcat/bin/startup.sh
 
 echo "=== [startserver.sh] Tomcat started ===" >> "$LOG_FILE"
