25233077da76aff2036387371a02f1b7ed6f2f5f
dhaneesh <dhaneeshjadhav@gmail.com>
2025-06-17 06:16:21 +0900

    dhan

--- scripts/startserver.sh
+++ scripts/startserver.sh
@@ -5,6 +5,6 @@ LOG_FILE="/tmp/deploy.log"
 
 echo "=== [startserver.sh] Starting Tomcat service ===" >> "$LOG_FILE"
 
-sudo  /opt/tomcat/bin/startup.sh
+sudo  /opt/bin/startup.sh
 
 echo "=== [startserver.sh] Tomcat started ===" >> "$LOG_FILE"
--- scripts/stopserver.sh
+++ scripts/stopserver.sh
@@ -1,5 +1,5 @@
 if id "tomcat" &>/dev/null; then
-  sudo -u tomcat /opt/tomcat/bin/shutdown.sh
+  sudo -u tomcat /opt/bin/shutdown.sh
 else
   echo "Tomcat user not found, skipping shutdown"
 fi
